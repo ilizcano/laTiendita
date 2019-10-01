252a30cecca8f38ef40ec8aab3452ca25ca0a684
ilizcano <56046781+ilizcano@users.noreply.github.com>
2019-10-01 15:15:10 -0700

    Original Code
    Original Code

---
+++ LaTiendita/Controllers/ReportesController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LaTiendita.Controllers
+{
+    public class ReportesController : Controller
+    {
+        // GET: Reporte
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ LaTiendita/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LaTiendita.Startup))]
+namespace LaTiendita
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
